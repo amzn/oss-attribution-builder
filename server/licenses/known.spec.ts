9251bfba0897ad642fa12753febaf1ed51b8638a
Jacob Peddicord <peddicor@amazon.com>
2017-08-21 14:33:36 -0700

    Fix tests
    Whoops. And yes, I did just turn on protected branches so that
    shouldn't happen again.

--- server/licenses/known.spec.ts
+++ server/licenses/known.spec.ts
@@ -12,6 +12,7 @@
  * permissions and limitations under the License.
  */
 
+import { List } from 'immutable';
 import { licenses } from './index';
 
 describe('known licenses', function () {
@@ -21,8 +22,8 @@ describe('known licenses', function () {
       const license = (name);
       expect(license).toBeDefined();
 
-      expect(data.get('tags')).toBe(true);
-      expect(typeof data.get('text')).toEqual('string');
+      expect(List.isList(data.get('tags'))).toBe(true, `license ${name}'s tags property was not a list`);
+      expect(typeof data.get('text')).toEqual('string', `license ${name}'s text was not a string`);
     }
   });
 
